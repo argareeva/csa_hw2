ea3e9f1ecfe442b1e9c42ad1297e163a168db10c
argareeva <argareeva@edu.hse.ru>
2022-12-11 20:50:20 +0500

    Directory with files is created. Method listOfFiles is showing every file in our directory in console. If there is subfolder - it goes inside and get names of inner files.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,37 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
+    static List<File> files;
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        listOfFiles("1", files);
+    }
+    public static void listOfFiles(String directoryName, List<File> files) {
+        File directory = new File(directoryName);
+        if(directory.exists()) {
+            File[] fList = directory.listFiles();
+            if (fList != null) {
+                for (File file : fList) {
+                    if (file.isFile()) {
+                        if (files == null) {
+                            files = new ArrayList<>();
+                            files.add(file);
+                        } else {
+                            files.add(file);
+                        }
+                    } else if (file.isDirectory()) {
+                        listOfFiles(file.getAbsolutePath(), files);
+                    }
+                }
+            }
+            assert fList != null;
+            for (File f : fList) {
+                System.out.println(f.getName());
+            }
+        } else {
+            System.out.println("Sorry, wrong name of directory!");
+        }
     }
 }
